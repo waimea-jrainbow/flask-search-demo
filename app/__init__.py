6d60f6588eec20f897a74f0fd8a922a2c125ce88
Jaxon Rainbow <jdrainbow@waimea.school.nz>
2026-06-03 15:13:05 +1200

    searching is done

--- app/__init__.py
+++ app/__init__.py
@@ -34,15 +34,72 @@ def show_welcome():
 @app.get("/creatures")
 def show_all_creatures():
     with connect_db() as db:
+        # Get the creatures to show
         sql = """
             SELECT id, species, name
             FROM creatures
         """
         params = ()
         creatures = db.execute(sql, params).fetchall()
+        
+        # Get the species for the drop-down in search
+        sql = """
+            SELECT DISTINCT species
+            FROM creatures
+            ORDER BY species ASC
+        """
+        params = ()
+        species_rows = db.execute(sql, params).fetchall()
+        # Turn into flat list
+        species_list = [species['species'] for species in species_rows]
 
-        return render_template("pages/creature_list.jinja", creatures=creatures)
+        return render_template("pages/creature_list.jinja", creatures=creatures, species_list=species_list)
 
+
+#-----------------------------------------------------------
+# Search the creatures
+#-----------------------------------------------------------
+@app.get("/search")
+def process_search():
+    search_term = request.args.get('q', '')          # fixed: args not arg
+    search_match = f"%{search_term}%"
+    species_term = request.args.get('species', '')   # fixed: args not arg
+    
+    with connect_db() as db:
+        # Build query depending on whether species filter is active
+        if species_term and species_term != 'all':
+            sql = """
+                SELECT id, species, name
+                FROM creatures
+                WHERE (name LIKE ? OR species LIKE ?)
+                AND species = ?
+            """
+            params = (search_match, search_match, species_term)
+        else:
+            sql = """
+                SELECT id, species, name
+                FROM creatures
+                WHERE name LIKE ? OR species LIKE ?
+            """
+            params = (search_match, search_match)
+
+        creatures = db.execute(sql, params).fetchall()
+
+        sql = """
+            SELECT DISTINCT species
+            FROM creatures
+            ORDER BY species ASC
+        """
+        species_rows = db.execute(sql, ()).fetchall()
+        species_list = [species['species'] for species in species_rows]
+
+    return render_template(
+        "pages/creature_list.jinja",
+        creatures=creatures,
+        species_list=species_list,
+        search_term=search_term,
+        species_term=species_term
+    )
 
 #-----------------------------------------------------------
 # Help page - Show some help
